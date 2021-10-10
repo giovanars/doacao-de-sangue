cd2fee1edc9982c4f0e67a4825486fe9f4ceb4e0
Michael Silva <michael.shel96@gmail.com>
2021-10-10 00:09:58 -0300

    Add back-end Api com Spring Boot

---
+++ api/doacaodesangue/src/main/java/br/com/doacaodesangue/DoacaodesangueApplication.java
@@ -0,0 +1,13 @@
+package br.com.doacaodesangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoacaodesangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoacaodesangueApplication.class, args);
+	}
+
+}
---
+++ api/doacaodesangue/src/test/java/br/com/doacaodesangue/DoacaodesangueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.doacaodesangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoacaodesangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
